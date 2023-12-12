987c5535c1c89df17a73581fd8db863f82f205eb
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2023-12-12 15:02:53 +0330

    02 - Color Picker Compelete

--- MAUIProject/Color Picker/MainPage.xaml.cs
+++ MAUIProject/Color Picker/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 {
     public partial class MainPage : ContentPage
     {
+        bool isRandom = false;
         public MainPage()
         {
             InitializeComponent();
@@ -9,17 +10,32 @@
 
         private void sld_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-
+            if (!isRandom)
+            {
+            var red = sldRed.Value;
+            var green = sldGreen.Value;
+            var blue = sldBlue.Value;
+            Color color = Color.FromRgb(red, green, blue);
+            SetColor(color);
+            }
         }
-
-        private void ImageButton_Clicked(object sender, EventArgs e)
-        {
-
-        }
-
         private void btnRandom_Clicked(object sender, EventArgs e)
         {
+            isRandom = true;
 
+            Random random = new();
+            var color = Color.FromRgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+            sldRed.Value = color.Red;
+            sldGreen.Value = color.Green;
+            sldBlue.Value = color.Blue;
+            SetColor(color);
+            isRandom = false;
+        }
+        private void SetColor(Color color)
+        {
+            Container.BackgroundColor = color;
+            btnRandom.BackgroundColor = color;
+            lblHex.Text = color.ToHex();
         }
     }
 
